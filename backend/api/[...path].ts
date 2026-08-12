27413849835d88081bad2190fc10d64a39759ec9
Arun Solo <YOUR_SPEC_ACCOUNT_EMAIL>
2026-08-12 22:15:11 +0530

    Initialize database in serverless entry points

--- backend/api/[...path].ts
+++ backend/api/[...path].ts
@@ -1,4 +1,8 @@
 import app from "../src/app";
 import { initializeDependencies } from "../src/api/dependencies";
+import { initDatabase } from "../src/database";
+
+await initDatabase();
 initializeDependencies();
+
 export default app;
